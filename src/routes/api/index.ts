5dfb9778941bb5b75bfa96139acd41f865a47d4a
Hayam Alrashed <113795738+HayamAlrashed@users.noreply.github.com>
2022-10-24 10:00:04 +0300

    Create index.ts

---
+++ src/routes/api/index.ts
@@ -0,0 +1,54 @@
+import logger from '../log/logger';
+import path from 'path';
+import { validation } from '../../middlewares/validation';
+import { Router, Request, Response } from 'express';
+import imgex from '../../middlewares/testimgex';
+
+const routes = Router();
+const imgSharp = require("../imgsharp");
+const fs = require('fs');
+
+try{
+
+  routes.get('/',logger,validation, (req: Request, res: Response) => { 
+    const name =  String(req.query.img);
+    const width = Number(req.query.width);
+    const height = Number(req.query.height);
+
+var data = require('../../../img/img.json');
+const arr = data.img[0];
+const imgfile = `${name}-width${width}-height${height}`;
+const imgpath = `img/edited/${imgfile}.jpeg`;
+
+if(imgex(name))
+{
+  if (fs.existsSync(imgpath)) {
+    res.sendFile(imgpath,{ root: '.' });
+  } else{
+    for (var i in arr){
+        if(i == name)
+        {
+            imgSharp.imgSharp(arr[name],name,Number(width),Number(height));
+            
+            setTimeout(() => {
+              res.sendFile(imgpath,{ root: '.' });
+            }, 2000);
+            console.log('check');
+        } 
+    }
+  }
+}
+else {res.send(`Image ${name} not exist`)}
+
+
+
+});
+
+
+}
+
+catch(error){console.log(error);}
+
+export default routes;
+
+
