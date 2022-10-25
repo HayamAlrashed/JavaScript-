6afe6ab37e3db83066ebde5de72b7ebf2c3a16a1
Hayam Alrashed <113795738+HayamAlrashed@users.noreply.github.com>
2022-10-25 21:36:01 +0300

    Update testimgex.ts

--- src/middlewares/testimgex.ts
+++ src/middlewares/testimgex.ts
@@ -1,18 +1,14 @@
-
-
-function imgex(name:string) {
-var data = require('../../img/img.json');
-const arr = data.img[0];
-var x = false;
-for (var i in arr){
-    if(i == name)
-        {
-            x = true;
-            return x;
-        }
+function imgex(name: string): boolean {
+  const data = require('../../img/img.json');
+  const arr = data.img[0];
+  let x = false;
+  for (let i in arr) {
+    if (i == name) {
+      x = true;
+      return x;
     }
-    return x;
-
+  }
+  return x;
 }
 export default imgex;
 // module.exports = {imgex};
