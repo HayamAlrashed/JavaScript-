6d8688ec3cd57e73a2c4c32fef324dbc24820498
Hayam Alrashed <113795738+HayamAlrashed@users.noreply.github.com>
2022-10-25 21:35:22 +0300

    Update validation.js

--- build/middlewares/validation.js
+++ build/middlewares/validation.js
@@ -1,15 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validation = void 0;
+//import imgex from '../middlewares/testimgex';
 var validation = function (req, res, next) {
     var img = String(req.query.img);
     var width = Number(req.query.width);
     var height = Number(req.query.height);
-    if (!img) {
-        return res.status(404).send('You should include image name -> use \'?img=\' parameter');
+    if (img == 'undefined') {
+        return res.status(404).send("You should include image name -> use '?img=' parameter");
     }
     else if (isNaN(height) || isNaN(width))
-        return res.status(400).send('img or width or height shoud be incloud and shoud be numbers');
+        return res.status(400).send('width or height shoud be incloud and shoud be ONLY numbers');
+    else if (height <= 0 || width <= 0)
+        return res.status(400).send('width or height shoud be positive numbers');
     next();
 };
 exports.validation = validation;
