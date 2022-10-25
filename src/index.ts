751c66389640cd0c29ff1c297081ae65da99c41f
Hayam Alrashed <113795738+HayamAlrashed@users.noreply.github.com>
2022-10-25 21:35:59 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,14 @@ import logger from './routes/log/logger';
 const app = express();
 const port = 3000;
 
-app.get('/',logger, (req, res) => {
-    res.send('Hello User.. You hava 2 imgs => cat dog ');
+app.get('/', logger, (req: express.Request, res: express.Response): void => {
+  res.send('Hello User.. You hava 2 imgs => cat dog ');
 });
 
-app.use('/api',routes)
+app.use('/api', routes);
 
-app.listen(port, ()=> {
-console.log(`server started at localhost:${port}`)
-   });
+app.listen(port, (): void => {
+  console.log(`server started at localhost:${port}`);
+});
 
-
-   export default {app};
+export default { app };
